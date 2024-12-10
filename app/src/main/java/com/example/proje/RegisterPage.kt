41734dfab09a55e5799ffbdd82cf04eb696aed13
semreerol <selmanemre147@gmail.com>
2024-12-10 18:19:00 +0300

    Files name upgraded

--- app/src/main/java/com/example/proje/RegisterPage.kt
+++ app/src/main/java/com/example/proje/RegisterPage.kt
@@ -3,21 +3,18 @@ package com.example.proje
 import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.example.proje.R // Arka plan resminizi buraya eklemelisiniz
 
 @Composable
 fun RegisterScreen(navController: NavController) {
